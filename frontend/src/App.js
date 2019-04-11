9065457e9e4d41329b1a1f5319ff3830f31d4d3f
Abby Gottlich <abby.gottlich@gmail.com>
2019-04-11 11:33:34 -0500

    image and text centered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
 
   loadImage() {
     if (this.state.returnedRestaurant.length > 0) {
-      return <img src={this.state.returnedRestaurant[0].image_url} alt={this.state.returnedRestaurant[0].categories[0].title} />
+      return <img className="resultImage" src={this.state.returnedRestaurant[0].image_url} alt={this.state.returnedRestaurant[0].categories[0].title} />
     }
   }
 
@@ -50,20 +50,26 @@ class App extends Component {
     const result = this.state.returnedRestaurant[0]
     return (
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
+        <form className="form" onSubmit={this.handleSubmit}>
           <input name="city" value={this.state.city} placeholder="City" onChange={this.handleChange} className="city-name" id="city-name"></input>
           <input name="USstate" value={this.state.USstate} placeholder="State" onChange={this.handleChange} className="state-name" id="state-name" maxLength="2"></input>
           <button type="submit">Find a Feast!</button>
         </form>
-        <div>{this.loadImage()}</div>
-        <div className="resultName">{restaurantLength > 0 ? result.name : ""}</div>
-        <div className="resultCategory">{restaurantLength > 0 ? result.categories[0].title : ""}</div>
-        <div className="resultPrice">{restaurantLength > 0 ? result.price : ""}</div>
-        <div className="resultLocation">{restaurantLength > 0 ? result.location.display_address.join(" ") : ""}</div>
-        <div className="hungryMessage">{restaurantLength <= 0 ? "Hungry?" : "Don't like your result? Click the 'Find Feast' button again!"}</div>
-        <div className="ratingMessage">{restaurantLength > 0 ? "Already been here? Give it a thumbs up or thumbs down and the rating will be saved to your profile!" : ""}</div>
-        <div className="thumbsUpButton">{restaurantLength > 0 ? "Thumbs Up" : ""}</div>
-        <div className="thumbsDownButton">{restaurantLength > 0 ? "Thumbs Down" : ""}</div>
+        <div className="restaurantInfo">
+          <div>{this.loadImage()}</div>
+          <div className="restaurantTextInfo">
+            <div className="resultName">{restaurantLength > 0 ? result.name : ""}</div>
+            <div className="resultCategory">{restaurantLength > 0 ? result.categories[0].title : ""}</div>
+            <div className="resultPrice">{restaurantLength > 0 ? result.price : ""}</div>
+            <div className="resultLocation">{restaurantLength > 0 ? result.location.display_address.join(" ") : ""}</div>
+          </div>
+        </div>
+        <div className="messagesAndButtons">
+          <div className="hungryMessage">{restaurantLength <= 0 ? "Hungry?" : "Don't like your result? Click the 'Find Feast' button again!"}</div>
+          <div className="ratingMessage">{restaurantLength > 0 ? "Already been here? Give it a thumbs up or thumbs down and the rating will be saved to your profile!" : ""}</div>
+          <div className="thumbsUpButton">{restaurantLength > 0 ? "Thumbs Up" : ""}</div>
+          <div className="thumbsDownButton">{restaurantLength > 0 ? "Thumbs Down" : ""}</div>
+        </div>
       </div>
     );
   }
