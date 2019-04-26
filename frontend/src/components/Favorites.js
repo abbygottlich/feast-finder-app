ffaaee893f9268fdbd335768f637623dbabc5228
Abby Gottlich <abby.gottlich@gmail.com>
2019-04-26 11:31:31 -0500

    edited favorites fetchratings path

--- frontend/src/components/Favorites.js
+++ frontend/src/components/Favorites.js
@@ -13,7 +13,7 @@ class Favorites extends Component {
     }
 
     fetchRatings = (rating) => {
-        fetch("/restaurants/" + rating)
+        fetch("/api/restaurants/" + rating)
             .then(res => res.json())
             .then(restaurants => {
                 this.setState({
