ad3dceafb06cbdf8335ebce429c15f879234f5a1
Abby Gottlich <abby.gottlich@gmail.com>
2019-04-26 10:43:26 -0500

    Updates

---
+++ precache-manifest.66b0da1e76c9184971f082153c01284a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "084764e698b026317693578102046767",
+    "url": "/feast-finder-app/static/media/x-icon.084764e6.svg"
+  },
+  {
+    "revision": "ee98966cd8fa55ba293d6670895e6c4b",
+    "url": "/feast-finder-app/static/media/menu.ee98966c.svg"
+  },
+  {
+    "revision": "bf3826b0fb5edbbc94665b7301d0989a",
+    "url": "/feast-finder-app/static/media/back-arrow.bf3826b0.svg"
+  },
+  {
+    "revision": "2d99c2f9e85a57348950",
+    "url": "/feast-finder-app/static/js/runtime~main.ae26ca81.js"
+  },
+  {
+    "revision": "facf803e7be1c2847bf5",
+    "url": "/feast-finder-app/static/js/main.7fa6b3e2.chunk.js"
+  },
+  {
+    "revision": "8797ae0f99b23ce09e35",
+    "url": "/feast-finder-app/static/js/2.7b0ed173.chunk.js"
+  },
+  {
+    "revision": "facf803e7be1c2847bf5",
+    "url": "/feast-finder-app/static/css/main.b72ddf9e.chunk.css"
+  },
+  {
+    "revision": "3e7b2bf20802f449209904d2e7c2aad2",
+    "url": "/feast-finder-app/index.html"
+  }
+];
